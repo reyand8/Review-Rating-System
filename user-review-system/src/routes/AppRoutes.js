77aabbfdd650c9fd385b45813632b4b02b7354f0
Andrey <andreeey.gg@gmail.com>
2024-10-07 17:51:23 +0200

    add AppRoutes

---
+++ user-review-system/src/routes/AppRoutes.js
@@ -0,0 +1,17 @@
+import {Route, Routes} from 'react-router-dom';
+
+import {ROUTES} from '../utils/routes';
+import NotFound from "../components/Status/NotFound";
+import Profile from "../components/Profile/Profile";
+import Home from "../components/Home/Home";
+
+
+const AppRoutes = () => (
+    <Routes>
+        <Route index element={<Home />} />
+        <Route path={ROUTES.PROFILE} element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+    </Routes>
+);
+
+export default AppRoutes;
