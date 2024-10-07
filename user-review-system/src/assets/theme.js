5bb66e26c367fc7e09fd8ddf6ccc990eaae4a439
Andrey <andreeey.gg@gmail.com>
2024-10-07 17:52:00 +0200

    add theme

---
+++ user-review-system/src/assets/theme.js
@@ -0,0 +1,69 @@
+import { createTheme } from "@mui/material/styles"; // Исправленный импорт
+
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1200,
+            xl: 1536,
+            xxl: 2160,
+        },
+    },
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#6e4ba2',
+            dark: '#9562ee',
+            light: '#d4bcf3',
+            contrastText: '#ffffff',
+            messageText: '#969595',
+        },
+        secondary: {
+            main: '#9c27b0',
+        },
+        error: {
+            main: '#d32f2f',
+            light: '#db5858',
+            dark: '#932020',
+            contrastText: '#ffffff',
+        },
+        success: {
+            main: '#2b792f',
+            light: '#559358',
+            dark: '#1e5420',
+            contrastText: '#ffffff',
+        },
+        warning: {
+            main: '#ed6c02',
+            light: '#f08934',
+            dark: '#a54b01',
+            messageWarning: '#969595',
+        },
+        background: {
+            default: '#cdb8ee',
+        },
+    },
+    typography: {
+        fontSize: 16,
+        fontWeightLight: 300,
+        h6: {
+            fontSize: 15,
+            fontWeight: 700,
+        },
+        h2: {
+            fontSize: 24,
+        },
+        subtitle2: {
+            fontWeight: 600,
+            lineHeight: 1.21,
+            fontSize: 14,
+        },
+        body1: {
+            fontWeight: 500,
+        },
+    },
+});
+
+export default theme;
