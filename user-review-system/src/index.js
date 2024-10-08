e367e48cd1e767cc8414d713b5263827695bafba
Andrey <andreeey.gg@gmail.com>
2024-10-08 11:20:03 +0200

    add store

--- user-review-system/src/index.js
+++ user-review-system/src/index.js
@@ -4,11 +4,15 @@ import {BrowserRouter} from "react-router-dom";
 
 import './index.css';
 import App from "./components/App";
+import {Provider} from "react-redux";
+import store from "./features/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+    </Provider>
 );
 
